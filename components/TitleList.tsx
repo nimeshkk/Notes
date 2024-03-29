a14dd288fd4e764ef80ef3392b79c8356dd6962c
nimeshkk <nimeshkliyanage@gmail.com>
2024-03-29 18:32:41 +0530

    update

--- components/TitleList.tsx
+++ components/TitleList.tsx
@@ -7,39 +7,44 @@ const fetchTitles = async () => {
     const response = await fetch("http://localhost:3000/api/titles", {
       cache: "no-store",
     });
-
     if (!response.ok) {
       throw new Error("Failed to fetch data from the server");
     }
-
-    return response.json(); 
+    return response.json();
   } catch (error) {
     console.error(error);
   }
 };
 
 export default async function TitleList() {
-  const titles = await fetchTitles(); 
-
+  const titles = await fetchTitles();
   return (
     <>
-      {titles.map((t:any) => (
-        <div
-          key={t._id} 
-          className="p-4 border border-slate-500 mt-2 flex justify-between gap-6 items-start"
-        >
-          <div>
-            <h1 className="font-bold text-2xl text-black">{t.title}</h1>
-            <p>{t.description}</p>
-          </div>
-          <div className="flex gap-2">
-            <Remove id={t._id}/>
-            <Link href={`/EditNote/${t._id}`}>
-              <BiEdit size={30} />
-            </Link>
-          </div>
-        </div>
-      ))}
+      {Array.isArray(titles) ? (
+        titles.length > 0 ? (
+          titles.map((t) => (
+            <div
+              key={t._id}
+              className="p-4 border border-slate-500 mt-2 flex justify-between gap-6 items-start"
+            >
+              <div>
+                <h1 className="font-bold text-2xl text-black">{t.title}</h1>
+                <p>{t.description}</p>
+              </div>
+              <div className="flex gap-2">
+                <Remove id={t._id} />
+                <Link href={`/EditNote/${t._id}`}>
+                  <BiEdit size={30} />
+                </Link>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No titles found.</p>
+        )
+      ) : (
+        <p>Error fetching titles.</p>
+      )}
     </>
   );
 }
